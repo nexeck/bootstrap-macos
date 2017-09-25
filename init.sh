80b770b348c4bbc64bbbd770e6374e87c52bf2e0
Marcel Beck <nexeck@MacBook-Pro.fritz.box>
2017-09-25 22:28:04 +0200

    Fix xcode installation withouth gui

--- init.sh
+++ init.sh
@@ -8,6 +8,28 @@ set +e
 
 echo ""
 echo "› Dependencies:"
+
+echo "  › Update Software:"
+sudo softwareupdate -i -a
+
+echo "  › Check & Install xcode:"
+
+# Only run if the tools are not installed yet
+# To check that try to print the SDK path
+xcode-select -p &> /dev/null
+if [ $? -ne 0 ]; then
+  echo "Xcode CLI tools not found. Installing them..."
+  touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
+  PROD=$(softwareupdate -l |
+    grep "\*.*Command Line" |
+    head -n 1 | awk -F"*" '{print $2}' |
+    sed -e 's/^ *//' |
+    tr -d '\n')
+  softwareupdate -i "$PROD" -v;
+else
+  echo "Xcode CLI tools OK"
+fi
+
 if ! command -v brew > /dev/null 2>&1; then
 	echo "  › Installing Homebrew"
 	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -15,8 +37,3 @@ else
     echo "  › Homebrew already installed"
 fi
 
-echo "  › Update Software:"
-sudo softwareupdate -i -a
-
-echo "  › Install xcode:"
-xcode-select --install
