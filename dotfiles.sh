67ac457f37882b353cbbae957a9cc108351febf0
Marcel Beck <marcel@beck.im>
2017-09-26 20:06:05 +0200

    Switch to ZSH Shell as default

--- dotfiles.sh
+++ dotfiles.sh
@@ -19,3 +19,9 @@ env RCRC=$HOME/.dotfiles/rcrc rcup
 
 echo "› Brew bundle"
 brew bundle --global
+
+echo "› ZSH"
+echo "  › Add ZSH to /etc/shells"
+test -n $(command -v zsh) && ! grep -qF "$(command -v zsh)" /etc/shells && command -v zsh | sudo tee -a /etc/shells
+echo "  › Switch default Shell to ZSH"
+chsh -s "$(command -v zsh)"
