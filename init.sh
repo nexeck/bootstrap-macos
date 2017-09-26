c5290b654540958f0bcd00f824b39fd42122d278
Marcel Beck <marcel@beck.im>
2017-09-26 18:44:35 +0200

    Remove xcode installation
    Homebrew will install xcode when it is missing

--- init.sh
+++ init.sh
@@ -12,24 +12,6 @@ echo "› Dependencies:"
 echo "  › Update Software:"
 sudo softwareupdate -i -a
 
-echo "  › Check & Install xcode:"
-
-# Only run if the tools are not installed yet
-# To check that try to print the SDK path
-xcode-select -p &> /dev/null
-if [ $? -ne 0 ]; then
-  echo "Xcode CLI tools not found. Installing them..."
-  touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
-  PROD=$(softwareupdate -l |
-    grep "\*.*Command Line" |
-    head -n 1 | awk -F"*" '{print $2}' |
-    sed -e 's/^ *//' |
-    tr -d '\n')
-  softwareupdate -i "$PROD" -v;
-else
-  echo "Xcode CLI tools OK"
-fi
-
 if ! command -v brew > /dev/null 2>&1; then
 	echo "  › Installing Homebrew"
 	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
