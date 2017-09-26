e84a83a3c4461ebe4dc1f91617841654f86ef6ab
Marcel Beck <marcel@beck.im>
2017-09-26 18:43:29 +0200

    Change script order
    Run dotfiles before defaults

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,8 +9,8 @@ set +e
 echo "› Init:"
 curl -fsSL https://raw.githubusercontent.com/nexeck/bootstrap-macos/master/init.sh | sh
 
-echo "› Defaults:"
-curl -fsSL https://raw.githubusercontent.com/nexeck/bootstrap-macos/master/defaults.sh | sh
-
 echo "› Dotfiles:"
 curl -fsSL https://raw.githubusercontent.com/nexeck/bootstrap-macos/master/dotfiles.sh | sh
+
+echo "› Defaults:"
+curl -fsSL https://raw.githubusercontent.com/nexeck/bootstrap-macos/master/defaults.sh | sh
